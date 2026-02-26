c7f428a82f3db10f841427961f44ca0255559643
Kian McKenna <kgmckenna04@gmail.com>
2026-02-25 18:23:54 -0600

    Add project files.

---
+++ Spring2026-Project2-kgmckenna/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Spring2026_Project2_kgmckenna.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
