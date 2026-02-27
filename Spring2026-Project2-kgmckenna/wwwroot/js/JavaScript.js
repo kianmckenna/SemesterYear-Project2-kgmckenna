297db99db802bbe82f58cf1ab0d97cdf3fd84654
Kian McKenna <kgmckenna04@gmail.com>
2026-02-27 15:47:17 -0600

    added time, formatted text, and outlined text

--- Spring2026-Project2-kgmckenna/wwwroot/js/JavaScript.js
+++ Spring2026-Project2-kgmckenna/wwwroot/js/JavaScript.js
@@ -1,3 +1,20 @@
-﻿$(function () {
-    console.log("jQuery is working!");
+﻿// retrieve current time
+$(function () {
+    $("#time").click(function () {
+        let now = new Date();
+        let currentTime = now.toLocaleTimeString();
+
+        $("#timeDisplay").text(currentTime);
+    });
+});
+
+// prevent form reload
+$(function () {
+    $("#searchForm").submit(function (event) {
+        event.preventDefault();
+
+        let query = $("#searchInput").val();
+        console.log("Searching for: ", query);
+
+    });
 });
